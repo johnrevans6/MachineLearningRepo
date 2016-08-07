832c23de3f5a9e2edff355c2afc1899a7d72fbdf
johnrevans6 <johnre@hawaii.edu>
2016-08-07 16:24:40 -0700

    Added basic frontend functionality

--- Stockmarket Classifier/Stockmarket Classifier/app.py
+++ Stockmarket Classifier/Stockmarket Classifier/app.py
@@ -3,6 +3,9 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
+import csv
+import urllib2
+import simplejson as json
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -11,9 +14,44 @@ wsgi_app = app.wsgi_app
 
 
 @app.route('/')
-def hello():
+def home():
     """Renders a sample page."""
-    return render_template("main.html")
+    stocks = preprocess()
+
+    return render_template("main.html",stocks=stocks)
+
+def preprocess():
+    symbols = []
+    with open('symbols.csv','rb') as file:
+        reader = csv.reader(file)
+        for symbol in reader:
+            symbols.append(symbol[0])
+
+    limit = 10.00
+    shares = 10
+    results = []
+         
+    for s in symbols:
+        response = urllib2.urlopen('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22'+s+'%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=')
+
+        result = json.loads(response.read())
+        price = result['query']['results']['quote']['LastTradePriceOnly']
+        if price == None:
+            continue
+        else:
+            price = float(price)
+        
+        if price * shares <= limit:
+            tmp = {'url': 'https://research.capitaloneinvesting.com/research/main/Stocks/Snapshot?symbol='+s,'symbol':s,'cost':price*shares}
+            results.append(tmp)
+             
+            
+            #results['symbol' == s]['cost'] = price * shares
+            
+        
+
+    return results
+
 
 if __name__ == '__main__':
     import os
